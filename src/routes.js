1c74424e2490526ddeaa6d2284882ea8a588f6a5
Gabriel Rodrigues <gabriel.rodrigues@mutantbr.com>
2020-02-08 17:51:51 -0300

    Database configuration and model user start

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Its work' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Gabriel',
+    email: 'gabriel@mail2.com',
+    password_hash: '123',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
