08115363b5d45e871add800c96eff192507eba2e
Gabriel Rodrigues <gabriel.rodrigues@mutantbr.com>
2020-02-23 14:14:25 -0300

    Listing appointment and applying pagination

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -3,8 +3,40 @@ import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      attributes: ['id', 'date'],
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -38,7 +70,9 @@ class AppointmentController {
     const hourStart = startOfHour(parseISO(date));
 
     if (isBefore(hourStart, new Date())) {
-      return res.status(400).json({ error: 'Past dates are not permitted' });
+      return res.status(400).json({
+        error: 'Past dates are not permitted',
+      });
     }
 
     const checkAvailability = await Appointment.findOne({
@@ -50,9 +84,9 @@ class AppointmentController {
     });
 
     if (checkAvailability) {
-      return res
-        .status(400)
-        .json({ error: 'Appointment date is not available' });
+      return res.status(400).json({
+        error: 'Appointment date is not available',
+      });
     }
 
     const appointment = await Appointment.create({
--- src/routes.js
+++ src/routes.js
@@ -24,6 +24,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.store);
 
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
